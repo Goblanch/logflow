06d68b01c44c8b464521aa210021412a88d4e073
Gonzalo Blanch Dominguez <gonzaloblanchdev@gmail.com>
2026-05-13 13:19:09 +0200

    #6: Implement FLogFlowEntry and FLowFlowSeverity (#37)

---
+++ Source/LogFlowCore/Private/LogFlowEntry.cpp
@@ -0,0 +1,16 @@
+﻿#include "LogFlowCore/Public/LogFlowEntry.h"
+
+FLogFlowEntry FLogFlowEntry::Create(
+	const FString& InMessage, 
+	ELogFlowSeverity InSeverity, 
+	FName InTag, 
+	FTimespan InTimestamp)
+{
+	FLogFlowEntry Entry;
+	Entry.Message = InMessage;
+	Entry.Severity = InSeverity;
+	Entry.Tag = InTag;
+	Entry.Timestamp = InTimestamp;
+	Entry.SystemTime = FDateTime::Now();
+	return Entry;
+}
