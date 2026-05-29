a411a0afde5974de98e9b5a154c94cff2b8d2e47
Gonzalo Blanch Dominguez <gonzaloblanchdev@gmail.com>
2026-05-29 15:52:07 +0200

    #26: Implement ULogFlowBlueprintLibrary (#57)
    * Implement ULogFlowBlueprintLibrary
    
    * Remove metadata from ULogFLowSubsystem::LogMessage()

--- Source/LogFlowBlueprintLibrary/Private/LogFlowBlueprintLibrary.cpp
+++ Source/LogFlowBlueprintLibrary/Private/LogFlowBlueprintLibrary.cpp
@@ -1,3 +1,20 @@
+#include "LogFlowBlueprintLibrary.h"
+#include "LogFlowSubsystem.h"
 #include "Modules/ModuleManager.h"
 
-IMPLEMENT_MODULE(FDefaultModuleImpl, LogFlowBlueprintLibrary)
+IMPLEMENT_MODULE(FDefaultModuleImpl, LogFlowBlueprintLibrary);
+
+void ULogFlowBlueprintLibrary::LogMessage(const FString& Message, FName Tag)
+{
+	ULogFlowSubsystem::LogMessage(Message, ELogFlowSeverity::Log, Tag);
+}
+
+void ULogFlowBlueprintLibrary::LogWarning(const FString& Message, FName Tag)
+{
+	ULogFlowSubsystem::LogMessage(Message, ELogFlowSeverity::Warning, Tag);
+}
+
+void ULogFlowBlueprintLibrary::LogError(const FString& Message, FName Tag)
+{
+	ULogFlowSubsystem::LogMessage(Message, ELogFlowSeverity::Error, Tag);
+}
